a690b1cfd5a88a99f653d9be868bb9c77b76608e
Paul Cortes <paulkerg@gmail.com>
2020-05-19 19:01:22 -0500

    Avances

--- Entidades/Escuela.cs
+++ Entidades/Escuela.cs
@@ -1,34 +1,46 @@
+using System;
+using System.Collections.Generic;
+using CoreEscuela.Util;
+using platzi_curso_csharp.Entidades;
+
 namespace CoreEscuela.Entidades
 {
-    class Escuela
+  public class Escuela : ObjetoEscuelaBase, ILugar
+  {
+    public int AñoDeCreación { get; set; }
+
+    public string Pais { get; set; }
+    public string Ciudad { get; set; }
+
+    public string Direccion { get; set; }
+    public TiposEscuela TipoEscuela { get; set; }
+    public List<Curso> Cursos { get; set; }
+
+    public Escuela(string nombre, int año) => (Nombre, AñoDeCreación) = (nombre, año);
+
+    public Escuela(string nombre, int año,
+         TiposEscuela tipo,
+         string pais = "", string ciudad = "") : base()
     {
-        string nombre;
-        public string Nombre
-        {
-            get { return "Copia:" + nombre; }
-            set { nombre = value.ToUpper(); }
-        }
-        public int AñoDeCreación { get; set; }
-
-        public string Pais { get; set; }
-        public string Ciudad { get; set; }
-        public TiposEscuela TipoEscuela { get; set; }
-
-        public Escuela(string nombre, int año) => (Nombre, AñoDeCreación) = (nombre, año);
-
-        public Escuela(string nombre, int año, 
-                       TiposEscuela tipo, 
-                       string pais = "", string ciudad = "")
-        {
-            (Nombre, AñoDeCreación) = (nombre, año);
-            Pais = pais;
-            Ciudad = ciudad;
-        }
-
-
-        public override string ToString()
-        {
-            return $"Nombre: \"{Nombre}\", Tipo: {TipoEscuela} {System.Environment.NewLine} Pais: {Pais}, Ciudad:{Ciudad}";
-        }
+      (Nombre, AñoDeCreación) = (nombre, año);
+      Pais = pais;
+      Ciudad = ciudad;
     }
+
+    public override string ToString()
+    {
+      return $"Nombre: \"{Nombre}\", Tipo: {TipoEscuela} {System.Environment.NewLine} Pais: {Pais}, Ciudad:{Ciudad}";
+    }
+
+    public void LimpiarLugar()
+    {
+      Printer.DrawLine();
+      Console.WriteLine("Limpiando Escuela...");
+      foreach (var curso in Cursos)
+      {
+        curso.LimpiarLugar();
+      }
+      Console.WriteLine($"Escuela {Nombre} Limpia");
+    }
+  }
 }
